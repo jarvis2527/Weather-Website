6167803620a41c6946bea12afc9fa164aaf7c13e
jarvis2527 <105741667+jarvis2527@users.noreply.github.com>
2024-06-13 13:08:43 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "1412813fd800dee4000173086ee24c2a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
